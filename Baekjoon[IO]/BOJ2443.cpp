fa0869476545a79819bb0d699884de5b61167ab0
홍성현 <sh0454@khu.ac.kr>
2018-08-16 12:00:05 +0900

    [UPDATE]BOJ2443

---
+++ Baekjoon[IO]/BOJ2443.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int input;
+	cin >> input;
+
+	for (int i = 0; i < input; i++)
+	{
+		for (int j = 0; j < i; j++)
+			cout << ' ';
+		for (int j = 2 * input; j > 2 * i + 1; j--)
+			cout << '*';
+		cout << "\n";
+	}
+
+	return 0;
+}
