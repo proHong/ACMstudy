6897bae2500361d214e258126c665dca1e1b74f1
tjdgus0454 <tjdgus0454@gmail.com>
2018-08-23 00:43:06 +0900

    [UPDATE]BOJ9095

---
+++ Baekjoon[DP]/BOJ9095.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+vector<int> temp;
+
+
+int dp(int n)
+{
+	if (n == 0)
+		return 0;
+	else if (n == 1)
+		return 1;
+	else if (n == 2)
+		return 2;
+	else if (n == 3)
+		return 4;
+	else if (temp[n] != -1)
+		return temp[n];
+	temp[n] = dp(n-3) + dp(n - 2) + dp(n - 1);
+	return temp[n];
+}
+
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		int n;
+		cin >> n;
+		temp.resize(n + 1, -1);
+		cout << dp(n) << '\n';
+	}
+
+
+
+	return 0;
+}
