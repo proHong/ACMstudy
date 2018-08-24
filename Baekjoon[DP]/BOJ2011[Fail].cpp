2e9964a3cf23fd83d7da7a056b694499e6e2757f
tjdgus0454 <tjdgus0454@gmail.com>
2018-08-24 14:36:02 +0900

    [UPDATE]BOJ2011[Fail]

---
+++ Baekjoon[DP]/BOJ2011[Fail].cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+long long d[5001];
+
+long long dp(string s)
+{
+	d[0] = 1;
+	d[1] = 1;
+
+	for (int i = 2; i <= s.length(); i++)
+	{
+		string st = s.substr(i - 2, 2);
+		int a = atoi(st.c_str());
+		if (a < 27 && s[i - 2] == '0')
+			d[i] = d[i - 1] % 1000000;
+		else if (a < 27 && s[i - 1] != '0')
+			d[i] = (d[i - 2] + d[i - 1]) % 1000000;
+		else if (s[i - 1] == '0' && a<27)
+			d[i] = d[i - 2] % 1000000;
+		else if (s[i - 1] == '0' && a>=27)
+			d[i] = 0;
+		else
+			d[i] = d[i - 1] % 1000000;
+	}
+	return d[s.length()];
+}
+
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+
+	string str;
+	cin >> str;
+
+	cout << dp(str) << '\n';
+
+
+	return 0;
+}
