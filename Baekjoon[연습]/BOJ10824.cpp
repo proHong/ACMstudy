d40abf98f2744a9be3e1c3743065280d4c32a0c9
tjdgus0454 <tjdgus0454@gmail.com>
2018-08-24 15:29:16 +0900

    [MODIFY]BOJ10824

--- Baekjoon[연습]/BOJ10824.cpp
+++ Baekjoon[연습]/BOJ10824.cpp
@@ -10,10 +10,10 @@ int main()
 	int num[4];
 	cin >> num[0] >> num[1] >> num[2] >> num[3];
 	string a, b;
-	
+
 	a = to_string(num[0]) + to_string(num[1]);
 	b = to_string(num[2]) + to_string(num[3]);
-	cout << atoi(a.c_str()) + atoi(b.c_str());
+	cout << atoll(a.c_str()) + atoll(b.c_str()) << '\n';
 
 	return 0;
 }
