066e2f0e01580b4e86830054616c33374b97a824
tjdgus0454 <tjdgus0454@gmail.com>
2018-08-26 17:24:15 +0900

    [MODIFY]BOJ2011

--- Baekjoon[DP]/BOJ2011.cpp
+++ Baekjoon[DP]/BOJ2011.cpp
@@ -8,6 +8,8 @@ long long d[5001];
 
 long long dp(string s)
 {
+	if (s[0] == '0' && s.length() == 1)
+		return 0;
 	d[0] = 1;
 	d[1] = 1;
 
@@ -19,9 +21,9 @@ long long dp(string s)
 			d[i] = d[i - 1] % 1000000;
 		else if (a < 27 && s[i - 1] != '0')
 			d[i] = (d[i - 2] + d[i - 1]) % 1000000;
-		else if (s[i - 1] == '0' && a<27)
+		else if (s[i - 1] == '0' && a < 27)
 			d[i] = d[i - 2] % 1000000;
-		else if (s[i - 1] == '0' && a>=27)
+		else if (s[i - 1] == '0' && a >= 27)
 			d[i] = 0;
 		else
 			d[i] = d[i - 1] % 1000000;
