7396436c75e3c0b0d716b74cc952de1ba6f27308
tjdgus0454 <tjdgus0454@gmail.com>
2018-08-26 17:01:36 +0900

    [MODIFY]BOJ11652

--- Baekjoon[연습]/BOJ11652.cpp
+++ Baekjoon[연습]/BOJ11652.cpp
@@ -2,36 +2,31 @@
 #include <algorithm>
 using namespace std;
 
+long long num[1000001];
 
 int main()
 {
 	int input = 0;
 	cin >> input;
 
-	long long * num = new long long[input];
-
 	for (int i = 0; i < input; i++)
 		cin >> num[i];
-	
+
 	sort(num, num + input);
 
 	long long number = num[0];
-	int a = 0, b = 0;
+	long long cnt1 = 1, cnt2 = 1;
 
-	for (int i = 0; i < input; i++)
+	for (int i = 1; i < input; i++)
 	{
-		if (number == num[i])
-			a++;
+		if (num[i - 1] == num[i])
+			cnt1++;
 		else
+			cnt1 = 1;
+		if (cnt1 > cnt2)
 		{
-			if (a > b)
-			{
-				number = num[i - 1];
-				b = a;
-				a = 1;
-			}
-			else
-				a = 1;
+			cnt2 = cnt1;
+			number = num[i];
 		}
 	}
 	cout << number << endl;
