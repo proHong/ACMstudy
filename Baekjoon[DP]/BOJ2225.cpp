425ad0d047eeb510d6782a84b7f73e6d24b69382
tjdgus0454 <tjdgus0454@gmail.com>
2018-08-26 17:31:02 +0900

    [MODIFY]BOJ2225

--- Baekjoon[DP]/BOJ2225.cpp
+++ Baekjoon[DP]/BOJ2225.cpp
@@ -6,20 +6,18 @@ using namespace std;
 
 long long d[202][202];
 
-long long dp(int n,int k)
+long long dp(int n, int k)
 {
 	for (int i = 0; i <= n; i++)
 		d[0][i] = 0;
 	for (int i = 0; i <= n; i++)
 		d[1][i] = 1;
-	for (int i = 0; i <= n; i++)
-		d[2][i] = i+1;
-	for (int i = 3; i <= k; i++)
+	for (int i = 2; i <= k; i++)
 	{
 		for (int j = 0; j <= n; j++)
 		{
 			for (int t = 0; t <= j; t++)
-				d[i][j] = (d[i][j] + d[i-1][j - k]) % 1000000000;
+				d[i][j] = (d[i][j] + d[i - 1][j - t]) % 1000000000;
 		}
 	}
 
@@ -36,7 +34,7 @@ int main()
 	int N, K;
 	cin >> N >> K;
 
-	cout << dp(N,K) << '\n';
+	cout << dp(N, K) << '\n';
 
 
 	return 0;
