5d853fd8213ac87456cf0a3c9488bcaebb1dea35
tjdgus0454 <tjdgus0454@gmail.com>
2018-09-11 22:16:04 +0900

    [UPDATE]BOJ11662[Fail]

---
+++ Baekjoon[이분탐색,삼분탐색]/BOJ11662[Fail].cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <queue>
+#include <algorithm>
+using namespace std;
+#pragma warning(disable:4996)
+
+
+pair<int, int> mins;
+pair<int, int> mine;
+pair<int, int> kangs;
+pair<int, int> kange;
+
+double bs()
+{
+	
+}
+
+int main()
+{
+	scanf("%d %d", &mins.first,&mins.second);
+	scanf("%d %d", &mine.first,&mine.second);
+	scanf("%d %d", &kangs.first,&kangs.second);
+	scanf("%d %d", &kange.first,&kange.second);
+
+
+	printf("%lf\n", bs());
+
+	return 0;
+}
